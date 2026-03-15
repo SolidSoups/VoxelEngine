0b7bf24d1e7f696bfe95c802af1a57824ddc72d9
SolidSoups <eli.marc.brown@gmail.com>
2026-03-16 00:25:01 +0100

    Added GLM to the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <print>
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 int main()
 {
+  glm::vec2 myVec2{1.0f};
   std::print("Hello C++23 World!");
 
   return 0;
