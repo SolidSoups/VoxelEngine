be2d00c48d447b0de06c767288b2a8a1751d27a4
SolidSoups <eli.marc.brown@gmail.com>
2026-03-16 17:18:20 +0100

    feat: ApplicationLayer

---
+++ src/ApplicationLayer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+struct GLFWwindow;
+
+class ApplicationLayer {
+public:
+  static bool Initialize(const std::string& aWindowTitle, int aWindowWidth,
+                         int aWindowHeight);
+  static void Resize(int aNewWindowWidth, int aNewWindowHeight);
+  static bool ShouldClose();
+  static void SwapBuffers();
+  static void CloseApplication();
+  static void Destroy();
+private:
+  static std::string myWindowTitle; 
+  static int myWindowHeight, myWindowWidth;
+  static GLFWwindow* myWindow;
+  static bool myShouldClose;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,49 +3,23 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
+#include "ApplicationLayer.h"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define WINDOW_TITLE "Voxel Playground"
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
-
 int main() {
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  // create the window
-  GLFWwindow *window =
-      glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
-  if (window == NULL) {
-    std::print("Failed to create GLFW window");
-    glfwTerminate();
-    return -1;
-  }
-  glfwMakeContextCurrent(window);
-
-  // Load glad
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::print(std::cerr, "Failed to initialize GLAD");
+  if(!ApplicationLayer::Initialize(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT)){
     return -1;
   }
 
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-  while (!glfwWindowShouldClose(window)) {
-    glfwSwapBuffers(window);
+  while (!ApplicationLayer::ShouldClose()) {
     glfwPollEvents();
+
+    ApplicationLayer::SwapBuffers();
   }
 
-  std::print("Hello C++23 World!");
-
-  glfwTerminate();
+  ApplicationLayer::Destroy();
   return 0;
 }
