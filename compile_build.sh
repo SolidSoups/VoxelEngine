cf7213ded4c5d0e367ff7514a479ee69702a376c
SolidSoups <eli.marc.brown@gmail.com>
2026-03-19 13:12:50 +0100

    made compile_build script simpler

--- compile_build.sh
+++ compile_build.sh
@@ -2,5 +2,10 @@
 
 set -e
 
-./compile.sh
-./build.sh
+if [ ! -d "./build" ]; then
+  mkdir build
+fi
+
+cmake -B build -S . -G Ninja -Wno-dev -Wno-deprecated -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+ninja -C build
