21cb0d25c5c3b6f01a96d7f05ec8156348b9c07a
SolidSoups <eli.marc.brown@gmail.com>
2026-03-22 16:02:29 +0100

    shader now displays world axis in grid

--- shaders/grid_frag.glsl
+++ shaders/grid_frag.glsl
@@ -4,11 +4,27 @@ out vec4 fragColor;
 in vec3 FragPos;
 in vec3 Normal;
 
-uniform vec3 color = vec3(1.0, 0.0, 0.0);
+uniform vec3 redColor = vec3(1.0, 0.0, 0.0);
+uniform vec3 greenColor = vec3(0.0, 1.0, 0.0);
+uniform vec3 blueColor = vec3(0.0, 0.0, 1.0);
+uniform vec3 grayColor = vec3(0.2, 0.2, 0.2);
 uniform vec3 uCameraPos;
+
+bool approx(float x, float y){
+  const float pm = 0.13f;
+  return
+    x > y - pm && x < y + pm;
+}
 
 void main() {
   float viewDot = dot(normalize(uCameraPos - FragPos), Normal);
+  vec3 color = grayColor;
+  if(approx(FragPos.x, 0.0) && approx(FragPos.y, 0.0))
+    color = redColor;
+  else if(approx(FragPos.z, 0.0) && approx(FragPos.y, 0.0))
+    color = blueColor;
+  else if(approx(FragPos.x, 0.0) && approx(FragPos.z, 0.0))
+    color = greenColor;
 
   fragColor = vec4(color, clamp(viewDot, 0.0, 1.0));
 }
