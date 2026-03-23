df5cab970a9faada7d72991777b63385cf235ca9
SolidSoups <eli.marc.brown@gmail.com>
2026-03-23 21:20:17 +0100

    added Mesh struct

---
+++ src/objects/Mesh.h
@@ -0,0 +1,26 @@
+#pragma once
+
+struct Mesh {
+  unsigned int vao = 0;
+  unsigned int vbo = 0;
+  unsigned int ebo = 0;
+
+public:
+  Mesh(const std::vector<float> &someVertices,
+       const std::vector<unsigned int> &someIndices);
+  ~Mesh();
+
+  // Delete copy
+  Mesh(const Mesh &) = delete;
+  Mesh &operator=(const Mesh &) = delete;
+
+  // Define move
+  Mesh(Mesh &&other) noexcept;
+  Mesh &operator=(Mesh &&other) noexcept;
+
+public:
+  void Draw();
+
+private:
+  size_t myElementsSize;
+};
