a4ffd60959f917392a4c717d3f4b28296b3d0d7f
SolidSoups <eli.marc.brown@gmail.com>
2026-04-04 17:45:41 +0200

    added icons for voxel types and brushes

--- src/editors/BrushEditor.cpp
+++ src/editors/BrushEditor.cpp
@@ -5,10 +5,33 @@
 #include <glm/glm.hpp>
 #include <imgui.h>
 #include "editors/ImGuiHelpers.h"
+#include "rendering/Texture.h"
+#include "voxel/VoxelType.h"
 #include "voxel/VoxelPainter.h"
 
+#define PALETTE_BUTTON_SIZE ImVec2(32, 32)
+#define BUTTON_COUNT 4
+#define BRUSH_COUNT 1
+
+BrushEditor::~BrushEditor() = default;
+
+BrushEditor::BrushEditor(){
+  myPenTexture = std::make_unique<Texture>("res/pen.png");
+  myEmptyVoxelTexture = std::make_unique<Texture>("res/Voxel_None.png");
+  myStoneVoxelTexture = std::make_unique<Texture>("res/Voxel_Stone.png");
+  mySandVoxelTexture = std::make_unique<Texture>("res/Voxel_Sand.png");
+  myWaterVoxelTexture = std::make_unique<Texture>("res/Voxel_Water.png");
+}
+
 void BrushEditor::Draw() {
+
   ImGui::Begin("Voxel Painter");
+
+  DrawPalette();
+  ImGui::Dummy(ImVec2(0, 5));
+  DrawBrushes();
+  ImGui::Dummy(ImVec2(0, 5));
+
 
   DrawVoxelType();
   DrawBrushType();
@@ -57,6 +80,62 @@ void BrushEditor::Draw() {
   ImGui::End();
 }
 
+void BrushEditor::DrawPalette(){
+  auto ImagePaletteButton = [&](const char* id, Texture&tex, VoxelType type){
+    bool selected = (mySelectedVoxelType == type);
+    if(selected)
+      ImGui::PushStyleColor(ImGuiCol_Button, ImGui::GetStyleColorVec4(ImGuiCol_ButtonActive));
+    if(ImGui::ImageButton(id, (ImTextureRef)(intptr_t)tex.id, PALETTE_BUTTON_SIZE)){
+      mySelectedVoxelType = type;
+      auto& painterState = PainterState::Get();
+      painterState.color = (VoxelType)mySelectedVoxelType;
+    }
+    if(selected)
+      ImGui::PopStyleColor();
+  };
+  float totalWidth = BUTTON_COUNT * PALETTE_BUTTON_SIZE.x +
+                     (BUTTON_COUNT - 1) * ImGui::GetStyle().ItemSpacing.x +
+                     ImGui::GetStyle().FramePadding.x * 2 * BUTTON_COUNT;
+  float totalHeight = PALETTE_BUTTON_SIZE.y + ImGui::GetStyle().FramePadding.y * 2;
+
+
+  ImGui::Text("Voxel Palette");
+  ImGui::BeginChild("##TypePalette", ImVec2(totalWidth, totalHeight));
+  ImagePaletteButton("##EmptyVoxel", *myEmptyVoxelTexture, VoxelType_EMPTY);
+  ImGui::SameLine();
+  ImagePaletteButton("##StoneVoxel", *myStoneVoxelTexture, VoxelType_STONE);
+  ImGui::SameLine();
+  ImagePaletteButton("##SandVoxel", *mySandVoxelTexture, VoxelType_SAND);
+  ImGui::SameLine();
+  ImagePaletteButton("##WaterVoxel", *myWaterVoxelTexture, VoxelType_WATER);
+  ImGui::EndChild();
+}
+
+
+void BrushEditor::DrawBrushes(){
+  auto ImagePaletteButton = [&](const char* id, Texture&tex, BrushType type){
+    bool selected = (myBrushType == type);
+    if(selected)
+      ImGui::PushStyleColor(ImGuiCol_Button, ImGui::GetStyleColorVec4(ImGuiCol_ButtonActive));
+    if(ImGui::ImageButton(id, (ImTextureRef)(intptr_t)tex.id, PALETTE_BUTTON_SIZE)){
+      myBrushType = type;
+      auto& painterState = PainterState::Get();
+      painterState.brush = (BrushType)myBrushType;
+    }
+    if(selected)
+      ImGui::PopStyleColor();
+  };
+
+  float totalWidth = BRUSH_COUNT * PALETTE_BUTTON_SIZE.x +
+                     (BRUSH_COUNT - 1) * ImGui::GetStyle().ItemSpacing.x +
+                     ImGui::GetStyle().FramePadding.x * 2 * BRUSH_COUNT;
+  float totalHeight = PALETTE_BUTTON_SIZE.y + ImGui::GetStyle().FramePadding.y * 2;
+
+  ImGui::Text("Brushes");
+  ImGui::BeginChild("##BrushPalette", ImVec2(totalWidth, totalHeight));
+  ImagePaletteButton("##SphereBrush", *myPenTexture, BrushType_SPHERE);
+  ImGui::EndChild();
+}
 
 void BrushEditor::DrawVoxelType(){
   static int selectedTypeIdx = (int)mySelectedVoxelType;
--- src/editors/BrushEditor.h
+++ src/editors/BrushEditor.h
@@ -1,16 +1,32 @@
 #pragma once
 
+#include <memory>
+
+#include "Editor.h"
 #include "voxel/VoxelType.h"
 #include "voxel/PaintType.h"
 
-class BrushEditor{
+struct Texture;
+
+class BrushEditor : public Editor{
 public:
-  void Draw(); 
+  BrushEditor();
+  ~BrushEditor();
+  void Draw() override; 
 
 private:
   void DrawVoxelType();
   void DrawBrushType();
   void DrawIVec3(const char* aName, const char* aID, glm::ivec3& aVec3);
+  void DrawPalette();
+  void DrawBrushes();
   VoxelType mySelectedVoxelType = VoxelType_EMPTY;
   BrushType myBrushType = BrushType_VOXEL; 
+
+private: // textures
+  std::unique_ptr<Texture> myPenTexture;
+  std::unique_ptr<Texture> myEmptyVoxelTexture;
+  std::unique_ptr<Texture> myStoneVoxelTexture;
+  std::unique_ptr<Texture> mySandVoxelTexture;
+  std::unique_ptr<Texture> myWaterVoxelTexture;
 };
